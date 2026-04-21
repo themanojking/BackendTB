f2d00684bfb7a4a04f1991c7cae5cdab97067e3f
MANOJ KUMAR J <kmanoj71737@gmail.com>
2026-04-21 23:10:35 +0530

    fix vercel serverless setup

--- server.js
+++ server.js
@@ -101,6 +101,4 @@ app.use((err, req, res, next) => {
 /* ===================== SERVER ===================== */
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+export default app;
