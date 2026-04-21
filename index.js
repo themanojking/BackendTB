4780390c169fc00fd370495716d6f075406ab0c3
MANOJ KUMAR J <kmanoj71737@gmail.com>
2026-04-22 00:44:12 +0530

    add cors

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ connectDB();
 
 // middleware
 app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(helmet());
 
